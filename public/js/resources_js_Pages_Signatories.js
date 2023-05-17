b89775b2429460caa723f45f6133d3a7d608b361
Jan Andrianne Prollo <Mercadojan123@gmail.com>
2023-05-17 15:29:07 +0800

    Jan Updates

---
+++ public/js/resources_js_Pages_Signatories.js
@@ -0,0 +1,13 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Signatories"],{
+
+/***/ "./resources/js/Pages/Signatories":
+/*!****************************************!*\
+  !*** ./resources/js/Pages/Signatories ***!
+  \****************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
