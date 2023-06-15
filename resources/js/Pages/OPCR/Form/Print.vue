d39d4073dcf7c9ed775c7ced68ffbb91901058b5
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2023-06-15 10:17:31 +0800

    commit

--- resources/js/Pages/OPCR/Form/Print.vue
+++ resources/js/Pages/OPCR/Form/Print.vue
@@ -5,7 +5,6 @@
 
     <div class="row gap-10 masonry pos-r">
         <!--<h1>JASPER REPORTS</h1>-->
-        {{ link }}
         <iframe :src="link" style="width:100%; height:700px" />
     </div>
 </template>
