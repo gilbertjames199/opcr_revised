c4972163e10f4ef0a23af5c10c50f54d7b190a43
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2023-07-06 09:41:10 +0800

    commit

--- app/Http/Controllers/HGDGScoreController.php
+++ app/Http/Controllers/HGDGScoreController.php
@@ -228,14 +228,18 @@ class HGDGScoreController extends Controller
     public function getQuestions(Request $request){
         $box_number = $request->box_number;
         $sector	=$request->sector;
-        $questions = HGDGQuestion::where('checklist_id',$request->checklist_id)
-                        ->orderByRaw("CAST(SUBSTRING_INDEX(question_number, '.', 1) AS UNSIGNED)")
-                        ->get();
+        $checklist_id = $request->checklist_id;
         return [
             "box_number"=>$box_number,
             "sector"=>$sector,
-            "questions"=>$questions
+            "checklist_id"=>$checklist_id
         ];
+    }
+    public function allQuestions(Request $request){
+        $questions =HGDGQuestion::where('checklist_id',$request->checklist_id)
+                    ->orderByRaw("CAST(SUBSTRING_INDEX(question_number, '.', 1) AS UNSIGNED)")
+                    ->get();
+        return $questions;
     }
 
 }
