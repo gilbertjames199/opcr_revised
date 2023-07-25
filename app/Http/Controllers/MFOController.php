fa12bdaf31909812b37ed08467164de01e2a7d56
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2023-07-25 15:57:03 +0800

    commit

--- app/Http/Controllers/MFOController.php
+++ app/Http/Controllers/MFOController.php
@@ -161,10 +161,15 @@ class MFOController extends Controller
         $idn = auth()->user()->recid;
 
         $data = $this->model->select('major_final_outputs.mfo_desc','major_final_outputs.id', 'major_final_outputs.FFUNCCOD')
-                ->where('FFUNCCOD', '1121')
+                ->where('department_code', auth()->user()->department_code)
                 ->orderBy('created_at', 'desc')
                 ->paginate(10)
                 ->withQueryString();
+                // $data = $this->model->select('major_final_outputs.mfo_desc','major_final_outputs.id', 'major_final_outputs.FFUNCCOD')
+                // ->where('FFUNCCOD', '1121')
+                // ->orderBy('created_at', 'desc')
+                // ->paginate(10)
+                // ->withQueryString();
                 // ->join('fms.accountaccess AS acc', function ($join) use ($idn) {
                 //     $join->on(DB::raw('TRIM(acc.ffunccod)'), '=', 'major_final_outputs.FFUNCCOD')
                 //         ->where('acc.iduser', $idn);
@@ -184,7 +189,7 @@ class MFOController extends Controller
         //dd($result);
         // dd($access);
         return inertia('MFOs/Direct',[
-            "data"=>$paginatedResult,
+            "data"=>$data,
             "filters" => $request->only(['search']),
             'can'=>[
                 'can_access_validation' => Auth::user()->can('can_access_validation',User::class),
