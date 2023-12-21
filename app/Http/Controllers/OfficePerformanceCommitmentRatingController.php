13b016978d6d7e5639fc0c86384076c06009cea0
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2023-12-21 14:23:08 +0800

    commet

--- app/Http/Controllers/OfficePerformanceCommitmentRatingController.php
+++ app/Http/Controllers/OfficePerformanceCommitmentRatingController.php
@@ -1036,22 +1036,22 @@ class OfficePerformanceCommitmentRatingController extends Controller
                     $approver = 'Jayvee Tyron L. Uy';
                     $pos = 'Vice Governor';
                 }
-                $var_q = $item->rating_q;
-                $var_e = $item->rating_e;
-                $var_t = $item->rating_t;
+                // $var_q = $item->rating_q;
+                // $var_e = $item->rating_e;
+                // $var_t = $item->rating_t;
 
-                $div = 3;
-                if ($var_q < 1) {
-                    $div = $div - 1;
-                }
-                if ($var_e < 1) {
-                    $div = $div - 1;
-                }
-                if ($var_t < 1) {
-                    $div = $div - 1;
-                }
-                $sum = $var_q + $var_e + $var_t;
-                $ave_qet = $sum / $div;
+                // $div = 3;
+                // if ($var_q < 1) {
+                //     $div = $div - 1;
+                // }
+                // if ($var_e < 1) {
+                //     $div = $div - 1;
+                // }
+                // if ($var_t < 1) {
+                //     $div = $div - 1;
+                // }
+                // $sum = $var_q + $var_e + $var_t;
+                // $ave_qet = $sum / $div;
 
                 // dd("average: " . $ave);
                 return [
@@ -1082,7 +1082,7 @@ class OfficePerformanceCommitmentRatingController extends Controller
                     "date_now" => $date_now,
                     "approver" => $approver,
                     "position" => $pos,
-                    "ave_qet" => $ave_qet
+                    // "ave_qet" => $ave_qet
                     // "from_excel" => $item->from_excel,
                     // "mfo_idmfo" => $item->mfo_idmfo,
                     // "paps_idmfo" => $item->paps_idmfo
