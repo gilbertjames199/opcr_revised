05360922ebcb6c3cedda091b2e5605fda537ac8b
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2024-05-09 15:56:01 +0800

    commit

--- app/Http/Controllers/UserEmployeesController.php
+++ app/Http/Controllers/UserEmployeesController.php
@@ -121,7 +121,8 @@ class UserEmployeesController extends Controller
             'salary_grade' => $datum['salary_grade'],
             'employment_type' => $datum['employment_type'],
             'employment_type_descr' => $datum['employment_type_descr'],
-            'designate_department_code' => $datum['designate_department_code']
+            'designate_department_code' => $datum['designate_department_code'],
+            'active_status' => $datum['active_status']
         ];
     }
     public function saveUserCredentials($datum)
