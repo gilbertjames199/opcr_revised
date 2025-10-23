202ce029563c1ffc3f86bd947a2d72ccbd956f2e
gilbertjames199 <gustavopuy1996@gmail.com>
2025-10-23 11:18:19 +0800

    comment

--- app/Http/Controllers/RevisionPlanController.php
+++ app/Http/Controllers/RevisionPlanController.php
@@ -2065,7 +2065,7 @@ class RevisionPlanController extends Controller
             }
             // dd($plan);
             // dd($plan->paps->office->office);
-            // $source=$this->set_source($source);
+            $source=$this->set_source($source);
             if (mb_strlen($source, 'UTF-8') < 25) {
 
                 $chars = preg_split('//u', $source, -1, PREG_SPLIT_NO_EMPTY);
