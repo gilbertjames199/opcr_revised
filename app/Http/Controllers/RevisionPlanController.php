6b44e7e032c47e896c6f43ef4af127313f1bf8d6
gilbertjames199 <gustavopuy1996@gmail.com>
2026-04-14 16:31:04 +0800

    comment

--- app/Http/Controllers/RevisionPlanController.php
+++ app/Http/Controllers/RevisionPlanController.php
@@ -4198,7 +4198,7 @@ class RevisionPlanController extends Controller
         $html = preg_replace('/<br\s*\/?>/i', "\n\n", $html);
 
         // Replace images with a large blank gap so Jasper has room for the image area
-        $html = preg_replace('/<img[^>]*>/i', "\n\n\n\n\n\n\n" . str_repeat(' ', 400) . "\n\n\n\n\n\n\n\n\n", $html);
+        $html = preg_replace('/<img[^>]*>/i', "<br><br><br><br>", $html);
         // $html = preg_replace('/<img[^>]*>/i', '<div style="height: 200px;"></div>', $html);
 
 
