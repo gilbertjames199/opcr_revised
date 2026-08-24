60c9916d2759cec49eaca7ba69e192486d33cca9
gilbertjames199 <gustavopuy1996@gmail.com>
2026-08-24 09:53:19 +0800

    comment

--- app/Models/AIPIndividualApprover.php
+++ app/Models/AIPIndividualApprover.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AIPIndividualApprover extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $connection = "mysql";
     protected $table='aip_individual_approvers';
     protected $guarded = [
